931c2afd0d32a3a54e8dbbcdd7ccb79d06335a2b
lujfy <140223821+lujfy@users.noreply.github.com>
2024-03-26 23:34:10 +0700

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const PROJECTS = [
         "title": "Chat Clone",
         "smallTitle" : "Messenger clone" ,
         "imageSrc": "projects/2.png",
-        "description": "ứng dụng được tạo ra để nhắn tin gửi file ảnh cho người dùng khác , là 1 phiên bản lite của Messenger.",
+        "description": "mimic the ability of a chat App so you can send image , chat with friends.",
         "skills": ["React", "Firebase", "Node" ],
         "demo": "https://chat-clone-uoma.onrender.com",
         "source": "https://github.com/lujfy/chat-clone"
@@ -14,7 +14,7 @@ export const PROJECTS = [
         "title": "FullStack Todo App",
         "smallTitle" : "Task app" ,
         "imageSrc": "projects/3.png",
-        "description": "ghi nhớ những việc quan trọng mà bạn cần làm thông qua Todo App , ứng dụng được làm để có thể hiểu hơn về backend",
+        "description": "Like the name suggest , it remind you about your task , created in backend NodeJS , it wil need a litle time before running",
         "skills": ["React", "Express", "Node" , "MongoDB" ],
         "demo": "https://mern-task-app-zw6o.onrender.com",
         "source": "https://github.com/lujfy/mern-task-frontend"
@@ -24,7 +24,7 @@ export const PROJECTS = [
         "title": "NextBlog",
         "smallTitle" : "Corporations Blogging Website" ,
         "imageSrc": "projects/5.png",
-        "description": "Chia sẻ , viết nên những cau chuyện của bạn thông qua Next Blog , A Serverless Website ",
+        "description": "Write , share your story with Next Blog , A Serverless Website ",
         "skills": ["Nextjs" , "Node" , "MongoDB" , "Express" ],
         "demo": "https://main--nextblog5123.netlify.app/",
         "source": "https://github.com/lujfy/NextBlog"
@@ -34,7 +34,7 @@ export const PROJECTS = [
         "title": "Todo App",
         "smallTitle" : "Local Storage task app" ,
         "imageSrc": "projects/6.png",
-        "description": "Năng động hơn , làm chủ các công việc của bạn với task app này",
+        "description": "another todoApp but this time , I use local storage for quicker run time ",
         "skills": ["Reactjs"],
         "demo": "https://main--localtodoapp.netlify.app/",
         "source": "https://github.com/lujfy/local-todoApp"
